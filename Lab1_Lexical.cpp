893b7ea1d0cd316f6a157a6f43ad279013deb490
Rifat_Sayeed <rifatahmed21095@gmail.com>
2026-04-28 15:14:48 -0400

    Update Lab1_Lexical.cpp

--- Lab1_Lexical.cpp
+++ Lab1_Lexical.cpp
@@ -1,118 +1,69 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<string> keywords = {
-    "int", "float", "double", "char", "bool", "void", "if", "else", "then",
-    "endif", "while", "for", "do", "switch", "case", "static", "print", "return"
-};
-
-bool isKeyword(const string &s){
-    return find(keywords.begin(), keywords.end(), s) != keywords.end();
-}
-
-bool isOperator(char c){
-    string ops = "+-*/%=><!";
-    return ops.find(c) != string::npos;
-}
-
-bool isSpecial(char c){
-    string specials = "(){},;[]";
-    return specials.find(c) != string::npos;
-}
-
-bool isIdentifierStart(char c){
-    return isalpha(c) || c == '_' || c == '$';
-}
-
-bool isIdentifierChar(char c){
-    return isalnum(c) || c == '_' || c == '$';
-}
-
-int main(){
+int main() {
 
     string code, line;
-
     cout << "Enter code (type END to finish):\n";
 
-    // multi-line input
-    while(getline(cin, line)){
-        if(line == "END") break;
-        code += line + "\n";
+    while(getline(cin, line)) {
+        //if(line == "END") break;
+        code += line + " ";
     }
 
-    // remove comments
-    regex blockComment(R"(/\*[\s\S]*?\*/)");
-    code = regex_replace(code, blockComment, "");
+    vector<string> keywords = {"int","float","if","else","for","while","return"};
 
-    regex singleComment(R"(//.*)");
-    code = regex_replace(code, singleComment, "");
+    vector<string> ids, cons, keys;
+    vector<char> ops, sp;
 
-    vector<string> identifiers, constants, keywordlist;
-    vector<char> operators, specials;
+    string temp = "";
 
-    int n = code.size();
-
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < code.size(); i++) {
         char c = code[i];
 
-        if(isspace(c)) continue;
+        // word (identifier / keyword)
+        if(isalnum(c)) {
+            temp += c;
+        }
+        else {
+            if(temp != "") {
+                // check keyword
+                if(find(keywords.begin(), keywords.end(), temp) != keywords.end())
+                    keys.push_back(temp);
+                else if(isdigit(temp[0]))
+                    cons.push_back(temp);
+                else
+                    ids.push_back(temp);
 
-        // Identifier or keyword
-        if(isIdentifierStart(c)){
-            string word = "";
-            while(i < n && isIdentifierChar(code[i])){
-                word += code[i];
-                i++;
+                temp = "";
             }
-            i--;
 
-            if(isKeyword(word))
-                keywordlist.push_back(word);
-            else
-                identifiers.push_back(word);
-        }
+            // operator
+            if(c=='+'||c=='-'||c=='*'||c=='/'||c=='=')
+                ops.push_back(c);
 
-        // Constant
-        else if(isdigit(c)){
-            string num = "";
-            while(i < n && isdigit(code[i])){
-                num += code[i];
-                i++;
-            }
-            i--;
-            constants.push_back(num);
-        }
-
-        // Operator
-        else if(isOperator(c)){
-            operators.push_back(c);
-        }
-
-        // Special symbol
-        else if(isSpecial(c)){
-            specials.push_back(c);
+            // special symbol
+            if(c=='('||c==')'||c=='{'||c=='}'||c==';'||c==',')
+                sp.push_back(c);
         }
     }
 
-    // Output
-    cout << "\n=== Lexical Analysis Result ===\n";
-
-    cout << "Keywords: ";
-    for(auto &k : keywordlist) cout << k << " ";
+    cout << "\nKeywords: ";
+    for(auto x: keys) cout << x << " ";
 
     cout << "\nIdentifiers: ";
-    for(auto &id : identifiers) cout << id << " ";
+    for(auto x: ids) cout << x << " ";
 
     cout << "\nConstants: ";
-    for(auto &c : constants) cout << c << " ";
+    for(auto x: cons) cout << x << " ";
 
     cout << "\nOperators: ";
-    for(auto &op : operators) cout << op << " ";
+    for(auto x: ops) cout << x << " ";
 
     cout << "\nSpecial Symbols: ";
-    for(auto &sp : specials) cout << sp << " ";
+    for(auto x: sp) cout << x << " ";
 
-    cout << "\n";
+    cout << endl;
 
     return 0;
 }
